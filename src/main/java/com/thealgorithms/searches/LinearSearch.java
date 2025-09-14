44d64a0269b2cb23f8d223a3261958facb221358
Mohamed-Irfan-git <irfanmt29@gmail.com>
2025-09-14 12:46:19 +0530

    xml file changes

--- src/main/java/com/thealgorithms/searches/LinearSearch.java
+++ src/main/java/com/thealgorithms/searches/LinearSearch.java
@@ -34,4 +34,12 @@ public class LinearSearch implements SearchAlgorithm {
         }
         return -1;
     }
+
+    public static void main(String[] args) {
+        Integer[] arr = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+        int target = 6;
+        LinearSearch linearSearch = new LinearSearch();
+        int x = linearSearch.find(arr,target);
+        System.out.println(x);
+    }
 }
